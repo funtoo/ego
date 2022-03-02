6d2a687b33b33ec2311e28a339a92a35f757dc47
drobbins <drobbins@funtoo.org>
2022-03-02 00:58:12 +0000

    Merge pull request #7 in CORE/ego from ~VADIK/ego:FL-6103 to master
    Squashed commit of the following:
    
    commit da7d41f0f02f06a265633e18478af3dee5b3b331
    Author: Vadim Vygonets <vadik@vygo.net>
    Date:   Tue Jan 1 19:20:09 2019 +0100
    
        FL-6103: Fix boot-update skiplist

--- python/funtoo/boot/resolver.py
+++ python/funtoo/boot/resolver.py
@@ -72,7 +72,7 @@ class Resolver:
 		else:
 			return dev
 
-	def GetMatchingKernels(self, scanpath, globlist, skip=None):
+	def GetMatchingKernels(self, scanpath, globlist, skip=None, bare=False):
 		# find kernels in scanpath that match globs in globlist, and return them
 		found = []
 		if skip is None:
@@ -87,7 +87,10 @@ class Resolver:
 						continue
 					# append the matching kernel, and "" representing that no
 					# [-v] extension was used
-					found.append([match, "", os.path.getmtime(match)])
+					if bare:
+						found.append(match)
+					else:
+						found.append([match, "", os.path.getmtime(match)])
 			if base_glob != wild_glob:
 				for match in glob.glob(wild_glob):
 					if match not in skip and match not in found:
@@ -96,7 +99,10 @@ class Resolver:
 							continue
 						# append the matching kernel, and the literal [-v]
 						# extension that was found on this kernel
-						found.append([match, match[len(scanpath) + 1 + pattern.find("[") :], os.path.getmtime(match)])
+						if bare:
+							found.append(match)
+						else:
+							found.append([match, match[len(scanpath) + 1 + pattern.find("[") :], os.path.getmtime(match)])
 		return found
 
 	def microcode_initialize(self):
@@ -392,7 +398,7 @@ class Resolver:
 				self.mount_if_necessary(scanpath)
 			if len(skiplist):
 				# find kernels to skip...
-				matches = self.GetMatchingKernels(scanpath, skiplist)
+				matches = self.GetMatchingKernels(scanpath, skiplist, bare=True)
 				skipmatch += matches
 			if len(findlist):
 				# find kernels to match (skipping any kernels we should skip...)
